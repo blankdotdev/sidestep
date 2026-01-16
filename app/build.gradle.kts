1b5c856c3021af379e46bf06465be58f25e6951b
blankdotdev <blankdotdev@users.noreply.github.com>
2026-01-16 00:17:59 -0500

    chore: bump version to 1.1.1 (vc3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.blankdev.sidestep"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
