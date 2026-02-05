e7cc66971f331d0ccb2eed4af0ce7f98dbb639a3
blankdotdev <blankdotdev@users.noreply.github.com>
2026-02-04 22:42:09 -0500

    Merge pull request #14 (Conflict Resolved)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,6 +90,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlin:kotlin-test:2.3.0")
     testImplementation("org.json:json:20251224")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 }
