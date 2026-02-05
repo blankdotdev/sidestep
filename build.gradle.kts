60e487894d848813abf35b173fbc61c5a2a3b201
blankdotdev <blankdotdev@users.noreply.github.com>
2026-02-05 01:32:38 -0500

    chore: downgrade Kotlin to 2.1.10 and revert CodeQL to manual build mode

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file
 plugins {
     id("com.android.application") version "9.0.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
-    id("io.gitlab.arturbosch.detekt") version "1.23.6" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
+    id("dev.detekt") version "2.0.0-alpha.2" apply false
 }
